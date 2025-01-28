cdb74591803240ec3109db2bb950a48ae1d6ff81
Subhodip Halder <166241247+subhodip100@users.noreply.github.com>
2025-01-28 20:52:26 +0530

    Add files via upload
    23/01/2025 lab_strings

---
+++ strings-1.23.25 assingment/Remove a newline.py
@@ -0,0 +1,10 @@
+# String with a newline
+string_with_newline = "Hello, World!\nWelcome to Anudip Python programming class."
+
+# Remove the newline using .replace()
+string_without_newline = string_with_newline.replace("\n", "")
+
+print("Original string:")
+print(string_with_newline)
+print("\nString without newline:")
+print(string_without_newline)
